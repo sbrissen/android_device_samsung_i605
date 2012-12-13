504216b2ac61fefa8e972cbc2d7fec1166f2680e
sbrissen <sbrissen@hotmail.com>
2012-12-13 11:29:12 -0500

    Initial Commit

---
+++ i605.mk
@@ -0,0 +1,116 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Include common makefile
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
+
+LOCAL_PATH := device/samsung/i605
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Init files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12 \
+    $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc \
+    $(LOCAL_PATH)/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
+    $(LOCAL_PATH)/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
+    $(LOCAL_PATH)/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
+	
+# Prebuilt Modules
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/exfat_core.ko:root/lib/modules/exfat_core.ko \
+    $(LOCAL_PATH)/prebuilt/exfat_fs.ko:root/lib/modules/exfat_fs.ko
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/tinyucm.conf:system/etc/tinyucm.conf \
+    $(LOCAL_PATH)/configs/default_gain.conf:system/etc/default_gain.conf \
+    $(LOCAL_PATH)/configs/Volume.db:system/etc/Volume.db \
+    $(LOCAL_PATH)/configs/audio_effects.conf:/system/etc/audio_effects.conf
+
+# Gps
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+
+# Camera FW
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/80cfw:system/etc/init.d/80cfw
+
+# Product specific Packages
+PRODUCT_PACKAGES += \
+    GalaxyNote2Settings \
+    SamsungServiceMode
+
+# NFC
+PRODUCT_PACKAGES += \
+    nfc.exynos4 \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag
+
+PRODUCT_COPY_FILES += \
+    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
+endif
+
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Stk
+
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.ril_class=SamsungCDMAQualcommRIL \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0,rmnet_usb0 \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10
+	
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/vzwapnlib.xml:system/etc/permissions/vzwapnlib.xml \
+	$(LOCAL_PATH)/configs/com.verizon.hardware.telephony.xml:system/etc/permissions/com.verizon.hardware.telephony.xml
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
+#Misc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/secomxregistry:/system/etc/secomxregistry \
+    $(LOCAL_PATH)/configs/security_profile.dat:/system/etc/security_profile.dat
+
+$(call inherit-product-if-exists, vendor/samsung/i605/i605-vendor.mk)

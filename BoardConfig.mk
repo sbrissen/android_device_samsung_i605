36875e758c1d772a8b340a389233800d9307407c
sbrissen <sbrissen@hotmail.com>
2013-01-18 13:30:42 -0500

    common all teh things - t0ltecdma

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,20 +17,10 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 
--include device/samsung/smdk4412-common/BoardCommonConfig.mk
-
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_i605_defconfig
+-include device/samsung/t0ltecdma/BoardCommonConfig.mk
 
 #Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i605/bluetooth
-
-# Audio
-BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
-BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
-COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := t0ltevzw,i605,SCH-I605
--- i605.mk
+++ i605.mk
@@ -14,98 +14,20 @@
 # limitations under the License.
 #
 
-# Include common makefile
-$(call inherit-product, device/samsung/smdk4412-common/common.mk)
-
 LOCAL_PATH := device/samsung/i605
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# This device is xhdpi.  However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
 # Init files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12 \
-    $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc \
-    $(LOCAL_PATH)/init.smdk4x12.usb.rc:root/init.smdk4x12.usb.rc \
-    $(LOCAL_PATH)/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
-    $(LOCAL_PATH)/init.bt.rc:root/init.bt.rc \
-    $(LOCAL_PATH)/lpm.rc:root/lpm.rc \
-    $(LOCAL_PATH)/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
-
-# Camera FW
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/80cfw:system/etc/init.d/80cfw
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/tinyucm.conf:system/etc/tinyucm.conf \
-    $(LOCAL_PATH)/configs/default_gain.conf:system/etc/default_gain.conf \
-    $(LOCAL_PATH)/configs/Volume.db:system/etc/Volume.db \
-    $(LOCAL_PATH)/configs/audio_effects.conf:/system/etc/audio_effects.conf
-
-# Product specific Packages
-PRODUCT_PACKAGES += \
-    GalaxyNote2Settings \
-    SamsungServiceMode
-
-# NFC
-PRODUCT_PACKAGES += \
-    nfc.exynos4 \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag
-
-PRODUCT_COPY_FILES += \
-    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    Stk \
-    libakm
-
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungCDMAQualcommRIL \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0,rmnet_usb0 \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10
+    $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12
 	
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/vzwapnlib.xml:system/etc/permissions/vzwapnlib.xml \
 	$(LOCAL_PATH)/configs/com.verizon.hardware.telephony.xml:system/etc/permissions/com.verizon.hardware.telephony.xml
 
-# These are the hardware-specific features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
-
-#Misc
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/secomxregistry:/system/etc/secomxregistry \
-    $(LOCAL_PATH)/configs/security_profile.dat:/system/etc/security_profile.dat
+# Include common makefile
+$(call inherit-product, device/samsung/t0ltecdma/t0ltecdma.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/i605/i605-vendor.mk)
